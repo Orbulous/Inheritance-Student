4a0ae7b1d4de5ba9e6c5b8045c3de1e2570043ed
Orbulous <118293080+Orbulous@users.noreply.github.com>
2023-05-15 10:27:03 -0700

    Slingyshot

---
+++ Assets/Scripts/Weapon/SlingShot.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : Weapon
+{
+    public GameObject projectilePrefab;
+    public override void Attack()
+    {
+        if (canAttack)
+        {
+            EnableWeapon();
+            boxCollider.enabled = true;
+            Invoke("DisableWeapon", attackDuration);
+            Invoke("AttackReset", 60f / attackRate);
+
+            // Create a new instance of the projectile prefab and set its position and rotation to match the slingshot.
+
+
+            GameObject projectileObject = Instantiate(projectilePrefab, transform.position, transform.rotation);
+
+
+
+            // Get the Projectile component on the new object
+            Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+            // Set the damage and speed of the projectile
+            projectile.damage = damage;
+            projectile.speed = 10f;
+
+        }
+    }
+
+}
